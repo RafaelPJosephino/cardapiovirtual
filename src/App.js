04298665bb8c362e07bff0e360c955b85d16dc6e
Rafael <Rafael.puckoski.josephino@gmail.com>
2023-11-15 11:58:35 -0300

    Fix whatsapp

--- src/App.js
+++ src/App.js
@@ -13,35 +13,35 @@ export class App extends Component {
           imagem: "https://drive.google.com/uc?id=1eSU_mWM6aMvZZ0-RfBE02mTC8So3g8Ju&export=download",
           preco: "(P) RS 25,00 (M) RS 50,00 (G) RS 75,00",
           categoria: 1,
-          descricao: ["400gm batata 400 gm frango sassami"]
+          descricao: ["400g de batata, 400g de frango sassami"]
         },
         {
           titulo: "Porção (2)",
           imagem: "https://drive.google.com/uc?id=1q7jx57n7bXFgzMDFnswncJ3LSDKawoJH&export=download",
           preco: "(P) RS 25,00 (M) RS 50,00 (G) RS 75,00",
           categoria: 1,
-          descricao: ["300gm batata, 250gm calabresa"]
+          descricao: ["300g de batata, 250g de calabresa"]
         },
         {
           titulo: "Porção (3)",
           imagem: "https://drive.google.com/uc?id=1zqzkDFCc7Ym52H0UTqDmaKILKP-E8V56&export=download",
           preco: "(P) RS 25,00 (M) RS 50,00 (G) RS 75,00",
           categoria: 1,
-          descricao: [" 400gm batata, 400gm frango a passarinho, 300gm polenta"]
+          descricao: [" 400g de batata, 400g de frango a passarinho, 300g de polenta"]
         },
         {
           titulo: "Porção (4)",
           imagem: "https://drive.google.com/uc?id=1jczUJnb8oOrbdNBRSSF1KI2W8b7_qsYp&export=download",
           preco: "(P) RS 25,00 (M) RS 50,00 (G) RS 75,00",
           categoria: 1,
-          descricao: ["300gm batata, 300gm tulipa, 400gm frango a passarinho,200gm coração"]
+          descricao: ["300g de batata, 300g de tulipa, 400g de frango a passarinho, 200g de coração"]
         },
         {
           titulo: "Porção (5)",
           imagem: "https://drive.google.com/uc?id=1gAUunz0k84NMyrW-0iDlITHKnfmaA7Av&export=download",
           preco: "(P) RS 25,00 (M) RS 50,00 (G) RS 75,00",
           categoria: 1,
-          descricao: ["400gm frango a passarinho, 400gm de batata"]
+          descricao: ["400g de frango a passarinho, 400g de batata"]
         },
         {
           titulo: "Porção (6)",
@@ -55,95 +55,95 @@ export class App extends Component {
           imagem: "https://drive.google.com/uc?id=1HFXeooo8B8xeLW1MGzSOeUUdkmEdqM8L&export=download",
           preco: "(P) RS 25,00 (M) RS 50,00 (G) RS 75,00",
           categoria: 1,
-          descricao: ["300gm de batata, 300gm polenta, 300gm frango sassami,300gm frango a passarinho"]
+          descricao: ["300g de de batata, 300g de polenta, 300g de frango sassami,300g de frango a passarinho"]
         },
         {
           titulo: "Porção (8)",
           imagem: "https://drive.google.com/uc?id=15ih_h0JCc_U9USQKlOAkQh3-uDwN7ZZS&export=download",
           preco: "(P) RS 25,00 (M) RS 50,00 (G) RS 75,00",
           categoria: 1,
-          descricao: ["300gm batata, 300gm tulipa"]
+          descricao: ["300g de batata, 300g de tulipa"]
         },
         {
           titulo: "Porção (9)",
           imagem: "https://drive.google.com/uc?id=1sK1o5xevETAP8oOKlTBYn4z2kgLUdgsa&export=download",
           preco: "(P) RS 25,00 (M) RS 50,00 (G) RS 75,00",
           categoria: 1,
-          descricao: ["300gm batata, 300gm polenta, 250gm calabresa, 300gm frango sassami, 200gm alcatra"]
+          descricao: ["300g de batata, 300g de polenta, 250g de calabresa, 300g de frango sassami, 200g de alcatra"]
         },
         {
           titulo: "Porção (10)",
           imagem: "https://drive.google.com/uc?id=1jc87xtnJyVRPfOh5SErYRJCXw_FnMS_9&export=download",
           preco: "(P) RS 25,00 (M) RS 50,00 (G) RS 75,00",
           categoria: 1,
-          descricao: ["250gm tiras alcatra, 250gm tiras file de peito, 250gm calabresa, 400gm batata"]
+          descricao: ["250g de tiras alcatra, 250g de tiras file de peito, 250g de calabresa, 400g de batata"]
         },
         {
           titulo: "Combo (1)",
           imagem: "https://drive.google.com/uc?id=1OiIDu4-QqUNWJNCtYHGQjLxPl1bLdIJy&export=download",
           preco: "(P) RS 25,00 (M) RS 50,00 (G) RS 75,00",
           categoria: 3,
-          descricao: ["Dois Hamburguer (pão, carne, queijo, presunto e tomate), 300gm batata, 300gm nagtes"]
+          descricao: ["Dois Hamburguer (pão, carne, queijo, presunto e tomate), 300g de batata, 300g de nagtes"]
         }, {
           titulo: "Batata frita",
           imagem: "https://drive.google.com/uc?id=1-tZF3WkvZ0VFRUVmMwc8-1P9TG4yw6dn&export=download",
           preco: "(P) RS 25,00 (M) RS 50,00 (G) RS 75,00",
           categoria: 1,
-          descricao: ["250gm batata","500gm batata"]
+          descricao: ["250g de batata","500g de batata"]
         }, {
           titulo: "Batata com bacon",
           imagem: "https://drive.google.com/uc?id=1ft_yotR9b5KhpmyHuZYqkBTXR9Olagop&export=download",
           preco: "(P) RS 25,00 (M) RS 50,00",
           categoria: 1,
-          descricao: ["250gm batata","500gm batata"]
+          descricao: ["250g de batata","500g de batata"]
         }, {
           titulo: "Batata bacon e cheddar",
           imagem: "https://drive.google.com/uc?id=1fSkAa0oiRgX-QPiAsqro6g9O4EwU2qMF&export=download",
           preco: "(P) RS 25,00 (M) RS 50,00",
           categoria: 1,
-          descricao: ["250gm batata","500gm batata"]
+          descricao: ["250g de batata","500g de batata"]
         }, {
           titulo: "Xis gaúcho (carne)",
           imagem: "https://drive.google.com/uc?id=1WgCZYrLzRjXoppkEp2RWfZiFhbUnQ_GE&export=download",
           preco: "RS 50,00",
           categoria: 2,
-          descricao: ["150gm carne, ovo, queijo, presunto, milho, ervilha, tomate picado, alface picado, maionese caseira acompanha fritas"]
+          descricao: ["150g de carne, ovo, queijo, presunto, milho, ervilha, tomate picado, alface picado, maionese caseira acompanha fritas"]
         }, {
           titulo: "Xis gaúcho (Frango)",
           imagem: "https://drive.google.com/uc?id=1WgCZYrLzRjXoppkEp2RWfZiFhbUnQ_GE&export=download",
           preco: "R$50,00",
           categoria: 2,
-          descricao: [" 150gm frango, ovo, queijo, presunto, milho, ervilha, tomate picado, alface picado, maionese caseira acompanha fritas"]
+          descricao: [" 150g de frango, ovo, queijo, presunto, milho, ervilha, tomate picado, alface picado, maionese caseira acompanha fritas"]
         }, {
           titulo: "Xis gaúcho (Calabresa)",
           imagem: "https://drive.google.com/uc?id=1WgCZYrLzRjXoppkEp2RWfZiFhbUnQ_GE&export=download",
           preco: "R$50,00",
           categoria: 2,
-          descricao: ["150gm calabresa, ovo, queijo, presunto, milho, ervilha, tomate picado, alface picado, maionese caseira acompanha fritas"]
+          descricao: ["150g de calabresa, ovo, queijo, presunto, milho, ervilha, tomate picado, alface picado, maionese caseira acompanha fritas"]
         }, {
           titulo: "Xis gaúcho (Bacon)",
           imagem: "https://drive.google.com/uc?id=1WgCZYrLzRjXoppkEp2RWfZiFhbUnQ_GE&export=download",
           preco: "R$50,00",
           categoria: 2,
-          descricao: ["150gm bacon, ovo, queijo, presunto, milho, ervilha, tomate picado, alface picado, maionese caseira acompanha fritas"]
+          descricao: ["150g de bacon, ovo, queijo, presunto, milho, ervilha, tomate picado, alface picado, maionese caseira acompanha fritas"]
         }, {
           titulo: "Xis gaúcho (Coração)",
           imagem: "https://drive.google.com/uc?id=1WgCZYrLzRjXoppkEp2RWfZiFhbUnQ_GE&export=download",
           preco: "R$50,00",
           categoria: 2,
-          descricao: ["150gm coração, ovo, queijo, presunto, milho, ervilha, tomate picado, alface picado, maionese caseira acompanha fritas"]
+          descricao: ["150g de coração, ovo, queijo, presunto, milho, ervilha, tomate picado, alface picado, maionese caseira acompanha fritas"]
         }, {
           titulo: "Xis gaúcho (Carne e Bacon)",
           imagem: "https://drive.google.com/uc?id=1WgCZYrLzRjXoppkEp2RWfZiFhbUnQ_GE&export=download",
           preco: "R$50,00",
           categoria: 2,
-          descricao: ["150gm carne, 100gm bacon, ovo, queijo, presunto, milho, ervilha, tomate picado, alface picado, maionese caseira acompanha fritas"]
+          descricao: ["150g de carne, 100g de bacon, ovo, queijo, presunto, milho, ervilha, tomate picado, alface picado, maionese caseira acompanha fritas"]
         }, {
           titulo: "Xis gaúcho (Tudão)",
           imagem: "https://drive.google.com/uc?id=1WgCZYrLzRjXoppkEp2RWfZiFhbUnQ_GE&export=download",
           preco: "R$50,00",
           categoria: 2,
-          descricao: ["Xis gaúcho (tudão), 150gm carne, 100gm bacon, 100gm calabresa, 100gm frango, 100gm coração, ovo, queijo, presunto, milho, ervilha, tomate picado, alface picado, maionese caseira acompanha fritas."]
+          descricao: ["Xis gaúcho (tudão), 150g de carne, 100g de bacon, 100g de calabresa, 100g de frango, 100g de coração, ovo, queijo, presunto, milho, ervilha, tomate picado, alface picado, maionese caseira acompanha fritas."]
         }
       ],
       categoria: 0,
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,18 +1,19 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import { Component } from "react"
 import Logo from "../Itens/imgs/Logo JNLanches.jpg"
+import LogoWhats from "../Itens/imgs/Whatsapp-pedido.png"
 import "./Navbar.css"
 
 class Navbar extends Component {
-  state = { 
+  state = {
     clicked: false,
-    categoriaSelecionada:0,
-   }
+    categoriaSelecionada: 0,
+  }
   handlerClick = () => {
     this.setState({ clicked: !this.state.clicked })
   }
   categoriaSelecionada = (cat) => {
-    this.setState({ categoriaSelecionada: cat})
+    this.setState({ categoriaSelecionada: cat })
   }
   render() {
     return (
@@ -22,18 +23,21 @@ class Navbar extends Component {
             <img src={Logo} className="logo-icon" alt="logo"></img>
           </a>
           <div>
-            <ul id="navbar" className={this.state.clicked? "#navbar active" :"#navbar"}>
+            <ul id="navbar" className={this.state.clicked ? "#navbar active" : "#navbar"}>
               <li>
-                <a  onClick={()=>{this.props.selecionarCategoria(0); this.categoriaSelecionada(0)}} className={this.state.categoriaSelecionada === 0 ? "active" : ""} >Todos</a>
+                <a onClick={() => { this.props.selecionarCategoria(0); this.categoriaSelecionada(0) }} className={this.state.categoriaSelecionada === 0 ? "active" : ""} >Todos</a>
               </li>
               <li>
-                <a  onClick={()=>{this.props.selecionarCategoria(1); this.categoriaSelecionada(1)}} className={this.state.categoriaSelecionada === 1 ? "active" : ""} >Porções</a>
+                <a onClick={() => { this.props.selecionarCategoria(1); this.categoriaSelecionada(1) }} className={this.state.categoriaSelecionada === 1 ? "active" : ""} >Porções</a>
               </li>
               <li>
-                <a  onClick={()=>{this.props.selecionarCategoria(2); this.categoriaSelecionada(2)}} className={this.state.categoriaSelecionada === 2 ? "active" : ""} >Hamburguers</a>
+                <a onClick={() => { this.props.selecionarCategoria(2); this.categoriaSelecionada(2) }} className={this.state.categoriaSelecionada === 2 ? "active" : ""} >Hamburguers</a>
               </li>
               <li>
-                <a  onClick={()=>{this.props.selecionarCategoria(3); this.categoriaSelecionada(3)}} className={this.state.categoriaSelecionada === 3 ? "active" : ""} >Combos</a>
+                <a onClick={() => { this.props.selecionarCategoria(3); this.categoriaSelecionada(3) }} className={this.state.categoriaSelecionada === 3 ? "active" : ""} >Combos</a>
+              </li>
+              <li>
+                <a href="https://api.whatsapp.com/send?phone=554884218223" target="_blank" rel="noreferrer"><img height="50px"  src={LogoWhats}  alt="" /></a>
               </li>
             </ul>
           </div>
